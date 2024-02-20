99249ea3529621547771ffcf0b1ab2c926b0e397
VLADASDAY <156774140+VLADASDAY@users.noreply.github.com>
2024-02-20 17:44:31 +0000

    Hw5

---
+++ Homeworkn5.py
@@ -0,0 +1,28 @@
+a1 = 5
+a2 = 50
+a3 = 500
+print('a1 =',5)
+print('a2 =',50)
+print('a3 =',500)
+
+b1 = 5
+b2 = 50
+b3 = 500
+print('b1 =',5)
+print('b2 =',50)
+print('b3 =',500)
+
+print('a1 == b1:', a1 == b1)
+print('a2 == b2:', a2 == b2)
+print('a3 == b3:', a3 == b3)
+
+print('a1 is b1:', a1 is b1)
+print('a2 is b2:', a2 is b2)
+print('a3 is b3:', a3 is b3)
+
+a4 = 1000
+b4 = 1000 
+
+print('a4 == b4:', a4 == b4)
+print('a4 is b4:', a4 is b4)
+print('a4+1 is b4+1:', a4+1 is b4+1)
