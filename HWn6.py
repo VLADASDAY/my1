76f35df961fb90b5a0d1d662e4b1a4a29a520543
VLADASDAY <156774140+VLADASDAY@users.noreply.github.com>
2024-03-05 17:11:27 +0000

    HW7

--- HWn6.py
+++ HWn6.py
@@ -2,4 +2,9 @@ v1 = 10
 v2 = 7
 v3 = 9
 
-print(min(v1, v2, v3))
+#print(min(v1, v2, v3))
+
+x = 1
+
+for i in range(5):
+    print(i)
---
+++ Homeworkn7.py
@@ -0,0 +1,7 @@
+x = "level"
+y = x[::-1]
+
+if x == y:
+    print(x, "is palindrom!")
+else:
+    print (x,"isnt palindrom")
