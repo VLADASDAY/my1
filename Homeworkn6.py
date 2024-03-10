74c8788bac5a616629b51e0f4569185a2731dbe8
VLADASDAY <156774140+VLADASDAY@users.noreply.github.com>
2024-03-10 15:01:36 +0000

    HWn7 (fix)

--- Homeworkn6.py
+++ Homeworkn6.py
@@ -1,11 +1,11 @@
 # drop sequential duplicated symbols 
 
-world = "coffeeeeeee" 
+word = "suuuuuuuuun" 
 word_new = ''
 prew_x = ''
 for x in word:
-    if x != prev_x:
-         prev_x = x 
+    if x != prew_x:
+         prew_x = x 
          word_new = word_new + x # word_new += x 
 
 print(word)
